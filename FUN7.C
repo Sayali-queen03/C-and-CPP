673acc35f060cc1be59526b921963aed22a82045
Sayali-queen03 <shelarsayali385@gmail.com>
2025-01-08 19:28:45 +0530

    Add files via upload

---
+++ FUN7.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void myFunction()
+{
+printf("Hello World\n");
+}
+int main()
+{
+clrscr();
+myFunction();
+myFunction();
+myFunction();
+getch();
+return 0;
+}
